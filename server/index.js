7bc3db63f14e4636b8940087324eb184f72518ba
Chris Patrick <atomize101@gmail.com>
2020-11-04 14:50:25 -0500

    Initial server code wired with mongooseDb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL =
+	'mongodb+srv://atomize101:123123Password@cluster0.mw1ik.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+	.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);

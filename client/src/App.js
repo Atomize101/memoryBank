21356424dbf4262f35c43fc23b151bf67ab6408b
Chris Patrick <atomize101@gmail.com>
2020-11-06 08:50:02 -0500

    Styling for components set up

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import memories from './images/memories.png';
 import Posts from '../src/components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+	const classes = useStyles();
+
 	return (
 		<Container maxWidth="lg">
-			<AppBar position="static" color="inherit">
-				<Typography varaint="h2" align="center">
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography className={classes.heading} varaint="h2" align="center">
 					MemoryBank
 				</Typography>
-				<img src={memories} alt="memories" height="60" />
+				<img className={classes.image} src={memories} alt="memories" height="60" />
 			</AppBar>
 			<Grow in>
 				<Container>

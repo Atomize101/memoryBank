1d23f9c7ccdab0291dcdfe8c26e60094e9a0e10c
Chris Patrick <atomize101@gmail.com>
2020-11-06 08:27:02 -0500

    post and form components wired up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import memories from './images/memories.png';
+import Posts from '../src/components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
 	return (
@@ -11,6 +13,18 @@ const App = () => {
 				</Typography>
 				<img src={memories} alt="memories" height="60" />
 			</AppBar>
+			<Grow in>
+				<Container>
+					<Grid container justify="space-between" alignItems="stretch" spaceing={3}>
+						<Grid item xs={12} sm={7}>
+							<Posts />
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Form />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
 		</Container>
 	);
 };

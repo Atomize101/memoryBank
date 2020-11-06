8f7da4ec70eee42d3b305b76fbf06e0419d44722
Chris Patrick <atomize101@gmail.com>
2020-11-06 07:56:55 -0500

    Working on react side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/memories.png';
 
 const App = () => {
-	return <div>Testing</div>;
+	return (
+		<Container maxWidth="lg">
+			<AppBar position="static" color="inherit">
+				<Typography varaint="h2" align="center">
+					MemoryBank
+				</Typography>
+				<img src={memories} alt="memories" height="60" />
+			</AppBar>
+		</Container>
+	);
 };
 
 export default App;

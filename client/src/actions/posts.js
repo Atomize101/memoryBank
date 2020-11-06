a4d794904062e5d82808fc5655dc3e4408e8a202
Chris Patrick <atomize101@gmail.com>
2020-11-06 12:15:00 -0500

    Passing payload to reducer

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -3,7 +3,11 @@ import * as api from '../api';
 // Action Creators
 
 export const getPosts = () => async (dispatch) => {
-	const action = { type: 'FETCH_ALL', payload: [] };
+	try {
+		const { data } = await api.fetchPosts();
 
-	dispatch(action);
+		dispatch({ type: 'FETCH_ALL', payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
 };

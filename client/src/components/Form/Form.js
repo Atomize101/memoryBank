5fc6fc2f5865f632da403bef71b7d798dc59f7a1
Chris Patrick <atomize101@gmail.com>
2020-11-07 06:40:00 -0500

    Setting up the form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './styles';
 
 const Form = () => {
 	const classes = useStyles();
-	return <h1>Form</h1>;
+	const [postData, setPostData] = useState({
+		creator: '',
+		title: '',
+		message: '',
+		tags: '',
+		selectedFile: '',
+	});
+
+	const handleSubmit = () => {};
+
+	return (
+		<Paper className={classes.paper}>
+			<form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+				<Typography variant="h6">Creating a Memory</Typography>
+				<TextField
+					name="creator"
+					variant="outlined"
+					label="Creator"
+					fullWidth
+					value={postData.creator}
+					onChange={(e) => setPostData({ creator: e.target.value })}
+				/>
+			</form>
+		</Paper>
+	);
 };
 
 export default Form;

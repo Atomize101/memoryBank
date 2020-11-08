3363858c4069a9aaef1f432d527fc382167cb27b
Chris Patrick <atomize101@gmail.com>
2020-11-08 11:20:17 -0500

    Customizing post cards

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import useStyles from './styles';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAl';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
-const Post = () => {
+const Post = ({ post }) => {
 	const classes = useStyles();
-	return <Card></Card>;
+	return (
+		<Card className={classes.card}>
+			<CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+			<div className={classes.overlay}>
+				<Typography variant="h6">{post.creator}</Typography>
+				<Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+			</div>
+			<div className={classes.overlay2}>
+				<Button style={{ color: 'white' }} size="small" onClick={() => {}}>
+					<MoreHorizIcon fontSize="default" />
+				</Button>
+			</div>
+			<div classname={classes.detals}>
+				<Typography variant="body2" color="textSecondary">
+					{post.creator}
+				</Typography>
+			</div>
+		</Card>
+	);
 };
 
 export default Post;

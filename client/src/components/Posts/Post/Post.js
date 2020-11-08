53474fd0f5659d9191fe06d25963effc6b808743
Chris Patrick <atomize101@gmail.com>
2020-11-08 10:43:55 -0500

    Working on displaying data to posts component

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAl';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const Post = () => {
 	const classes = useStyles();
-	return <h1>Post</h1>;
+	return <Card></Card>;
 };
 
 export default Post;

e5f1f7f34cf0d954496acabe3ef92a5ecace818e
Chris Patrick <atomize101@gmail.com>
2020-11-09 17:14:53 -0500

    env file

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -15,8 +17,7 @@ app.use('/posts', postRoutes);
 
 // https://www.mongodb.com/cloud/atlas
 
-const CONNECTION_URL =
-	'mongodb+srv://atomize101:123123Password@cluster0.mw1ik.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
